2946a05efc41b872c25a562f9f14186f51899b0a
alameen-alli <allialameen8739@gmail.com>
2024-02-25 14:37:30 +0100

    feat: add store changer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,27 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import React from "react";
 import MainNav from "./main-nav";
+import StoreChanger from "./store-changer";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
 
-export default function Navbar() {
+export default async function Navbar () {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
+        <StoreChanger items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
